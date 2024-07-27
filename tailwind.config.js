d0de708ca736917f994f43bd9e152020250e8cb5
gabibrevi <gabibrevi@gmail.com>
2024-07-27 16:46:45 +0200

    Project creation, tailwind install and set up.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import { jsx } from 'react/jsx-runtime'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
