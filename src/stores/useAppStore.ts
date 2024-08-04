d32cd0b35419cc4dda76e52a20445bf5efb4a04a
gabibrevi <gabibrevi@gmail.com>
2024-08-04 08:57:16 +0200

    Creacion de Storages

---
+++ src/stores/useAppStore.ts
@@ -0,0 +1,7 @@
+import { create } from 'zustand'
+
+import { createRecipeSlice, RecipeSliceType } from './recipeSlice'
+
+export const useAppStore = create<RecipeSliceType>((...a) => ({
+    ...createRecipeSlice(...a),
+}))
