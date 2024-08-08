210508b548f63d12e8d69d0698e535f7c0c57186
gabibrevi <gabibrevi@gmail.com>
2024-08-08 05:57:02 +0200

    Se agregó drinkCard a favoritesPage para mostrar los favoritos

--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,7 +1,22 @@
+import { useAppStore } from '../stores/useAppStore'
+import DrinkCard from '../components/DrinkCard'
+import { useMemo } from 'react'
+
 export default function FavoritesPage() {
+    const favorites = useAppStore((state) => state.favorites)
+    const hasFavorites = useMemo(() => favorites.length, [favorites])
     return (
         <>
-            <h1>Favorites</h1>
+            <h1 className=' text-6xl font-extrabold'>Favorites</h1>
+            {hasFavorites ? (
+                <div className=' grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 my-10 gap-10'>
+                    {favorites.map((drink) => (
+                        <DrinkCard key={drink.idDrink} drink={drink} />
+                    ))}
+                </div>
+            ) : (
+                <p className=' mt-40 text-center text-3xl'>Los favoritos se mostrarán aquí</p>
+            )}
         </>
     )
 }
